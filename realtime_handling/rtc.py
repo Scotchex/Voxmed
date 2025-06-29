26b777cd266c7663fdb7a87d418e4fd0cccdd462
Scotchex <barkodadam@gmail.com>
2025-06-29 21:15:01 +0300

    AI part done, next will work on the basic front end of the project

---
+++ realtime_handling/rtc.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request, render_template
+import requests
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+openai_api_key = os.getenv('OPENAI_API_KEY')
+assistant_id = os.getenv('ASSISTANT_ID')
+
+def get_session(prompt:str='You are a helpful agent'):
+    url = "https://api.openai.com/v1/realtime/sessions"
+
+    payload = {
+        'model': 'gpt-4o-realtime-preview-2024-12-17',
+        'modalities': ['audio', 'text'],
+        'instructions' : prompt,
+        'voice' : 'sage'
+    }
+
+    headers = {
+        'Authorization' : f'Bearer {openai_api_key}',
+        'Content-Type' : 'application/json'
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    return response.json()
